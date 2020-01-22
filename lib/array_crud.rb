e6ed29e7603098bdeec8a4dcd336f3722730bd63
Justin Pierce <jwpierce1984@gmail.com>
2020-01-22 13:32:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   ["water", "tea", "coffee", "soda"]
 end
 
-def add_element_to_end_of_array("create_an_array", "milk")
-  create_an_array << "milk"
+def add_element_to_end_of_array(array, element)
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
